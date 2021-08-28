8492aafed29cd4d19195a9c530803e1f1740792d
swanine <haohao1500@foxmail.com>
2021-08-28 20:18:05 +0800

    chore(extends): upgrade eslint and use plugin:prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
+    "plugin:prettier-recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020,
