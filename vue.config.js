7741856119c6a01f73acede16222a9de8ecddb6d
swanine <haohao1500@foxmail.com>
2021-09-12 20:05:06 +0800

    feat: vue_config add chainWebpack path_resolve

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+module.exports = {
+  outputDir: './build',
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       views: '@/views'
+  //     }
+  //   }
+  // }
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     views: '@/views'
+  //   }
+  // }
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('views', '@/views')
+  }
+}
