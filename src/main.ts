bccae9418c7844a7bebab071a9810414756faecf
swanine <haohao1500@foxmail.com>
2021-09-12 20:11:10 +0800

    feat: initialize the entry file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import swanRequest from './service'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-createApp(App).use(store).use(router).mount("#app");
-
-swanRequest.request({
-  url: '/home/multidata',
-  method: 'GET'
-})
+createApp(App).use(router).use(store).mount('#app')
