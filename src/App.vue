af913b532230d2bde09d780d8174a92ef79af067
swanine <haohao1500@foxmail.com>
2021-09-12 20:08:48 +0800

    feat(login): feat router layout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view />
+  <router-link to="/login">登录</router-link>
+  <router-link to="/main">首页</router-link>
+  <router-view></router-view>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+})
+</script>
 
 <style lang="less">
 #app {
@@ -13,18 +20,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>{{ $store.state.name }}</h2>
+  <div>login</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
