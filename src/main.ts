93b94dcfd2a1b4c48e96cca2f664d8b90c2d80c0
swanine <haohao1500@foxmail.com>
2021-09-13 13:50:55 +0800

    chore: add register of global request utils

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,21 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { globalRegister } from './global'
+import swanRequest from './utils'
 
-createApp(App).use(router).use(store).mount('#app')
+//全局引入
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css'
+
+const app = createApp(App)
+// globalRegister(app)
+app.use(router).use(store).use(globalRegister).mount('#app')
+
+const result = swanRequest.request({
+  url: '/home/multidata',
+  method: 'GET'
+})
+result.then((res) => {
+  console.log(res)
+})
