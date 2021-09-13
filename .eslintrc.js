b10d7f874564e8f8cafb27c9d6b8b080b7a67c31
swanine <haohao1500@foxmail.com>
2021-09-13 13:25:34 +0800

    style(type): @typescript-eslint/no-explicit-any

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    '@typescript-eslint/no-var-requires': 0
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-explicit-any': 0
   }
 }
