7773d726495df212951bbde91c8ec3aa63cb0878
swanine <haohao1500@foxmail.com>
2021-09-14 22:44:26 +0800

    fix: automatic component registration method

--- src/global/register-element.ts
+++ src/global/register-element.ts
@@ -1,9 +1,9 @@
 import type { App } from 'vue'
 
 import 'element-plus/theme-chalk/base.css'
-import { ElButton, ElInput } from 'element-plus'
+import { ElButton, ElInput, ElTabPane, ElTabs } from 'element-plus'
 
-const components = [ElButton, ElInput]
+const components = [ElButton, ElInput, ElTabs, ElTabPane]
 
 export default function (app: App): void {
   for (const component of components) {
