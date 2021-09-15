248cb12d75dd9d4dd6ade852a4dfcafb021701a5
swanine <haohao1500@foxmail.com>
2021-09-15 16:59:42 +0800

    chore: adjust the structure of

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,4 @@
 <template>
-  <router-link to="/login">登录</router-link>
-  <router-link to="/main">首页</router-link>
-  <el-button type="primary" plain>主要按钮</el-button>
-  <el-button type="success" plain>成功按钮</el-button>
-  <el-button type="info" plain>信息按钮</el-button>
-  <el-button type="warning" plain>警告按钮</el-button>
-  <el-button type="danger" plain>危险按钮</el-button>
   <router-view></router-view>
 </template>
 
@@ -20,11 +13,6 @@ export default defineComponent({
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
