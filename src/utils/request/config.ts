e6a31f4cbc38f90a4fa43d8d631ed9ff29f2bf5e
swanine <haohao1500@foxmail.com>
2021-09-15 16:53:29 +0800

    chore(env): proxy BASE_URL '/api'

--- src/utils/request/config.ts
+++ src/utils/request/config.ts
@@ -10,11 +10,11 @@ let BASE_URL = ''
 const TIME_OUT = 10000
 
 if (process.env.NODE_ENV === 'development') {
-  BASE_URL = 'http://123.207.32.32:8000/'
+  BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://swanine.xyz/prod'
+  BASE_URL = 'http://coderwhy.org/prod'
 } else {
-  BASE_URL = 'http://swanine.xyz/test'
+  BASE_URL = 'http://coderwhy.org/test'
 }
 
 export { BASE_URL, TIME_OUT }
