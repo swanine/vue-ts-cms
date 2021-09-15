c448d99ed2f46617cf2149fdf6797ad58bc61459
swanine <haohao1500@foxmail.com>
2021-09-15 16:51:53 +0800

    ci: add devServer proxy target

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,36 @@
-const path = require('path')
 module.exports = {
+  // 1.配置方式一: CLI提供的属性
   outputDir: './build',
-  publicPath: './',
-  // configureWebpack: {
-  //   resolve: {
-  //     alias: {
-  //       views: '@/views'
-  //     }
-  //   }
-  // }
+  // publicPath: './',
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://152.136.185.210:5000',
+        pathRewrite: {
+          '^/api': ''
+        },
+        changeOrigin: true
+      }
+    }
+  },
+  // 2.配置方式二: 和webpack属性完全一致, 最后会进行合并
+  configureWebpack: {
+    resolve: {
+      alias: {
+        components: '@/components'
+      }
+    }
+  }
   // configureWebpack: (config) => {
   //   config.resolve.alias = {
   //     '@': path.resolve(__dirname, 'src'),
-  //     views: '@/views'
+  //     components: '@/components'
   //   }
   // }
-  chainWebpack: (config) => {
-    config.resolve.alias
-      .set('@', path.resolve(__dirname, 'src'))
-      .set('views', '@/views')
-  }
+  // 3.配置方式三:
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('components', '@/components')
+  // }
 }
