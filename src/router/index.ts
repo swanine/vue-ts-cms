f5fe37925327bf8be4fbc5482a174ec41d9cf8c3
swanine <haohao1500@foxmail.com>
2021-09-17 21:06:26 +0800

    feat(router): change redirect '/login' to '/main'.add router beforeEach

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,11 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import type { RouteRecordRaw } from 'vue-router'
+import localCache from '@/utils/cache'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    redirect: '/login'
+    redirect: '/main'
   },
   {
     path: '/login',
@@ -19,6 +20,21 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
   routes,
   history: createWebHashHistory()
+})
+
+router.beforeEach((to) => {
+  if (to.path !== '/login') {
+    const token = localCache.getCache('token')
+    if (!token) {
+      return '/login'
+    }
+  }
+  if (to.path === '/login') {
+    const token = localCache.getCache('token')
+    if (token) {
+      return '/main'
+    }
+  }
 })
 
 export default router
