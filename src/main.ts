27ec224b1b4edbef32f598405d031f6bbb135de9
swanine <haohao1500@foxmail.com>
2021-09-17 21:03:44 +0800

    chore(main): import the daily entry file

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { globalRegister } from './global'
-import swanRequest from './utils'
+// import { globalRegister } from './global'
+import 'normalize.css'
+import './assets/css/base.less'
+import { setupStore } from '@/store/index'
 
 //全局引入
-// import ElementPlus from 'element-plus'
+import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 // globalRegister(app)
-app.use(router).use(store).use(globalRegister).mount('#app')
-
-const result = swanRequest.request({
-  url: '/home/multidata',
-  method: 'GET'
-})
-result.then((res) => {
-  console.log(res)
-})
+setupStore()
+app.use(router).use(store).use(ElementPlus).mount('#app')
