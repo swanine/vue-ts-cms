b57ebfe6b70ea7221e2d635fdbbb04c511a1d8c6
swanine <haohao1500@foxmail.com>
2021-09-19 19:58:08 +0800

    feat: add menu routing page other

---
+++ src/views/main/analysis/overview/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>overview</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/views/main/system/user/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <formInput />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import formInput from '@/components/form-input/index.vue'
+export default defineComponent({
+  setup() {
+    return {}
+  },
+  components: {
+    formInput
+  }
+})
+</script>
+
+<style lang="less" scoped></style>
