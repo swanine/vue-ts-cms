97d11622d5ba734af26b32bc067323b00e5ef946
swanine <haohao1500@foxmail.com>
2021-09-19 19:53:39 +0800

    feat: add 404 page

---
+++ src/views/not-found/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="not-found">
+    <h2>页面不小心走失了</h2>
+    <img src="~@/assets/images/404.svg" />
+    <el-button @click="backHome" type="danger" round>返回首页</el-button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default defineComponent({
+  setup() {
+    const router = useRouter()
+    function backHome() {
+      router.push('/main')
+    }
+    return {
+      backHome
+    }
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.not-found {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  h2 {
+    margin: 60px 0 0;
+    font-size: 50px;
+    color: #409eff;
+  }
+}
+img {
+  width: 800px;
+}
+</style>
