4a09e1fd2d1ee3540c2b6a4cdc33f1f70e6cdd53
swanine <haohao1500@foxmail.com>
2021-09-20 19:52:35 +0800

    fix(lang): example Change the default project language to zh_CN

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,10 @@ import { setupStore } from '@/store/index'
 
 //全局引入
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 // import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 // globalRegister(app)
 setupStore()
-app.use(router).use(store).use(ElementPlus).mount('#app')
+app.use(router).use(store).use(ElementPlus, { locale: zhCn }).mount('#app')
