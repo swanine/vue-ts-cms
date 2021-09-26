b3f0fa1702a46fb85ebba8850fd88ff776dc73b5
swanine <haohao1500@foxmail.com>
2021-09-26 19:06:47 +0800

    chore(scroll): display none scrollbar for main view

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -79,9 +79,12 @@ body > .el-container {
 }
 
 .page-wrapper {
-  overflow: hidden;
+  overflow-y: scroll;
   width: 100%;
   height: 100%;
   background-color: #ffffff;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 }
 </style>
