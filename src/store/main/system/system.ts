458e1e0c503c5e311f42336b72c9872cf9ddcac2
swanine <haohao1500@foxmail.com>
2021-09-28 22:09:55 +0800

    refactor(actions): extract a public function to get table page data

--- src/store/main/system/system.ts
+++ src/store/main/system/system.ts
@@ -9,7 +9,9 @@ const systemModule: Module<ISystemState, IRootState> = {
   state() {
     return {
       userList: [],
-      userCount: 0
+      userCount: 0,
+      roleList: [],
+      roleCount: 0
     }
   },
   mutations: {
@@ -18,18 +20,55 @@ const systemModule: Module<ISystemState, IRootState> = {
     },
     changeUserCount(state, userCount: number) {
       state.userCount = userCount
+    },
+    changeRoleList(state, roleList: any[]) {
+      state.roleList = roleList
+    },
+    changeRoleCount(state, roleCount: number) {
+      state.roleCount = roleCount
+    }
+  },
+  getters: {
+    // 匹配pageName返回对应State数据
+    pageListData(state) {
+      return (pageName: string) => {
+        switch (pageName) {
+          case 'User':
+            return state.userList
+          case 'Role':
+            return state.roleList
+        }
+      }
+    },
+    pageDataCount(state) {
+      return (pageName: string) => {
+        switch (pageName) {
+          case 'User':
+            return state.userCount
+          case 'Role':
+            return state.roleCount
+        }
+      }
     }
   },
   actions: {
     async getPageList({ commit }, payload: any) {
+      // 获取pageUrl
+      const pageName = payload.pageName
+      let pageUrl = ''
+      switch (pageName) {
+        case 'User':
+          pageUrl = '/users/list'
+          break
+        case 'Role':
+          pageUrl = '/role/list'
+          break
+      }
       // 对页面发送网络请求
-      const pageResult = await getPageListData(
-        payload.pageUrl,
-        payload.queryInfo
-      )
+      const pageResult = await getPageListData(pageUrl, payload.queryInfo)
       const { list, totalCount } = pageResult.data
-      commit('changeUserList', list)
-      commit('changeUserCount', totalCount)
+      commit(`change${pageName}List`, list)
+      commit(`change${pageName}Count`, totalCount)
     }
   }
 }
