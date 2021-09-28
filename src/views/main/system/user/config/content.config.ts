bed0ff30bc74ee0e591aaabaeb1b5f9bcec81175
swanine <haohao1500@foxmail.com>
2021-09-28 22:19:29 +0800

    feat: users page table config

---
+++ src/views/main/system/user/config/content.config.ts
@@ -0,0 +1,32 @@
+export const contentTableConfig = {
+  title: '用户数据',
+  // 表格头数据
+  propsList: [
+    { prop: 'name', label: '用户名', minWidth: '100' },
+    { prop: 'realname', label: '真实姓名', minWidth: '100' },
+    { prop: 'cellphone', label: '手机号码', minWidth: '100' },
+    { prop: 'enable', label: '状态', minWidth: '80', slotName: 'status' },
+    {
+      prop: 'createAt',
+      label: '创建时间',
+      minWidth: '180',
+      slotName: 'createAt'
+    },
+    {
+      prop: 'updateAt',
+      label: '更新时间',
+      minWidth: '180',
+      slotName: 'updateAt'
+    },
+    {
+      label: '操作',
+      minWidth: '120',
+      slotName: 'handle'
+    }
+  ],
+
+  // 显示表格序号
+  showIndexColumn: true,
+  // 行选中功能
+  showSelectColumn: true
+}
