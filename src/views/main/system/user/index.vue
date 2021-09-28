9958b4f0ec803db2fb5118cb29ea75d88b7ab4f9
swanine <haohao1500@foxmail.com>
2021-09-28 22:18:07 +0800

    feat: improve user page data display and data query

--- src/views/main/system/user/index.vue
+++ src/views/main/system/user/index.vue
@@ -1,24 +1,47 @@
 <template>
-  <div class="search-info">
-    <formInput />
-  </div>
+  <el-scrollbar max-height="620px">
+    <div class="search-info">
+      <formInput @handleQueryClick="handleQueryClick" />
+      <!-- 用户数据表格组件 -->
+      <page-content
+        :contentTableConfig="contentTableConfig"
+        pageName="User"
+        ref="refPageContent"
+      ></page-content>
+    </div>
+  </el-scrollbar>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+
 import formInput from '@/components/form-input/index.vue'
+import pageContent from '@/components/page-content'
+
+import { contentTableConfig } from './config/content.config'
+
 export default defineComponent({
   setup() {
-    return {}
+    const refPageContent = ref()
+    // form组件传递查询方法
+    function handleQueryClick(val: any) {
+      refPageContent.value.getPageData(val)
+    }
+    return {
+      contentTableConfig,
+      handleQueryClick,
+      refPageContent
+    }
   },
   components: {
-    formInput
+    formInput,
+    pageContent
   }
 })
 </script>
 
 <style lang="less" scoped>
 .search-info {
-  margin: 30px 0;
+  padding-top: 30px;
 }
 </style>
