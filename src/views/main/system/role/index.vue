fec40d388116bf737be39ef333c13a4be517fd45
swanine <haohao1500@foxmail.com>
2021-09-29 22:22:56 +0800

    feat: add system role page

--- src/views/main/system/role/index.vue
+++ src/views/main/system/role/index.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>role页面</div>
+  <el-scrollbar max-height="620px">
+    <!-- 用户数据表格组件 -->
+    <page-content
+      :contentTableConfig="contentTableConfig"
+      pageName="Role"
+    ></page-content>
+  </el-scrollbar>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import pageContent from '@/components/page-content'
+
+import { contentTableConfig } from './config/content.config'
+
 export default defineComponent({
   setup() {
-    return {}
+    return {
+      contentTableConfig
+    }
+  },
+  components: {
+    pageContent
   }
 })
 </script>
-
-<style lang="less" scoped></style>
