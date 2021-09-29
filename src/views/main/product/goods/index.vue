383bbc96e288c26a18f4764255c99293399728d2
swanine <haohao1500@foxmail.com>
2021-09-29 22:27:11 +0800

    feat: add product goods page

--- src/views/main/product/goods/index.vue
+++ src/views/main/product/goods/index.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>goods</div>
+  <el-scrollbar max-height="620px">
+    <!-- 商品数据表格组件 -->
+    <page-content
+      :contentTableConfig="contentTableConfig"
+      pageName="Goods"
+    ></page-content>
+  </el-scrollbar>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import pageContent from '@/components/page-content'
+
+import { contentTableConfig } from './config/content.config'
+
 export default defineComponent({
   setup() {
-    return {}
+    return {
+      contentTableConfig
+    }
+  },
+  components: {
+    pageContent
   }
 })
 </script>
-
-<style lang="less" scoped></style>
