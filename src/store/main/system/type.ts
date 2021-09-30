0453440db36c0aa344f0e146e8d5cbd156a3d298
swanine <haohao1500@foxmail.com>
2021-09-30 22:45:03 +0800

    feat(type): add menu state type

--- src/store/main/system/type.ts
+++ src/store/main/system/type.ts
@@ -1,8 +1,10 @@
 export interface ISystemState {
-  userList: any[]
-  userCount: number
+  usersList: any[]
+  usersCount: number
   roleList: any[]
   roleCount: number
   goodsList: any[]
   goodsCount: number
+  menuList: any[]
+  menuCount: number
 }
