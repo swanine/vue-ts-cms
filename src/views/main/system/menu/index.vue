4caf03a9c0eff7a419535871a6a2eff601a298a4
swanine <haohao1500@foxmail.com>
2021-10-01 22:47:39 +0800

    feat: add menu page

--- src/views/main/system/menu/index.vue
+++ src/views/main/system/menu/index.vue
@@ -1,15 +1,34 @@
 <template>
-  <div>menu页面</div>
+  <div class="menu-wrapper">
+    <!-- 商品数据表格组件 -->
+    <page-content
+      :contentTableConfig="contentTableConfig"
+      pageName="Menu"
+    ></page-content>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
+import pageContent from '@/components/page-content'
+
+import { contentTableConfig } from './config/content.config'
+
 export default defineComponent({
   setup() {
-    return {}
+    return {
+      contentTableConfig
+    }
+  },
+  components: {
+    pageContent
   }
 })
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.menu-wrapper {
+  background-color: #ffffff;
+}
+</style>
