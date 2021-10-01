290ef4af46e0d5dc4873c16af674496d8803728b
swanine <haohao1500@foxmail.com>
2021-10-01 22:49:40 +0800

    feat: base echarts component

---
+++ src/base-ui/base-echart/src/base-echart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    ref="echartDivRef"
+    class="base-echart"
+    :style="{ width: width, height: height }"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, withDefaults, defineProps, watchEffect } from 'vue'
+import { EChartsOption } from 'echarts'
+import useEcharts from '@/hooks/use-echarts'
+
+const echartDivRef = ref()
+// 定义props
+const props = withDefaults(
+  defineProps<{
+    options: EChartsOption
+    width?: string
+    height?: string
+  }>(),
+  {
+    width: '100%',
+    height: '210px'
+  }
+)
+
+onMounted(() => {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const { setOptions } = useEcharts(echartDivRef.value!)
+  watchEffect(() => {
+    setOptions(props.options)
+  })
+})
+</script>
