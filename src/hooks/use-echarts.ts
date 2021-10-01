2eac549fa20db5c04c2faf116c3fdccd403b6df5
swanine <haohao1500@foxmail.com>
2021-10-01 22:51:18 +0800

    feat(hooks): add use base echart hooks function

---
+++ src/hooks/use-echarts.ts
@@ -0,0 +1,17 @@
+import * as echarts from 'echarts'
+
+export default function (el: HTMLElement) {
+  const echartInstance = echarts.init(el)
+
+  const setOptions = (options: echarts.EChartsOption) => {
+    echartInstance.setOption(options)
+  }
+
+  window.addEventListener('resize', () => {
+    echartInstance.resize()
+  })
+  return {
+    echartInstance,
+    setOptions
+  }
+}
