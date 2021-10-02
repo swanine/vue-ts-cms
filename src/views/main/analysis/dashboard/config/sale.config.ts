4f6439ee940b9e405dc2d57dd2a5f7d9be23ff24
swanine <haohao1500@foxmail.com>
2021-10-02 17:41:35 +0800

    feat: card data component config

---
+++ src/views/main/analysis/dashboard/config/sale.config.ts
@@ -0,0 +1,31 @@
+export const goodsDataConfig = {
+  saleTitle: '商品总量',
+  saleIcon: 'el-icon-takeaway-box',
+  saleCrement: '4.07%',
+  color: '#2f49d1',
+  backgroundColor: '#eaedfa'
+}
+
+export const saleDataConfig = {
+  saleTitle: '销售总量',
+  saleIcon: 'el-icon-shopping-bag-2',
+  saleCrement: '9.27%',
+  color: '#ffb648',
+  backgroundColor: '#fff8ed'
+}
+
+export const favorDataConfig = {
+  saleTitle: '收藏总量',
+  saleIcon: 'el-icon-collection-tag',
+  saleCrement: '4.1%',
+  color: '#5f2eea',
+  backgroundColor: '#efeafd'
+}
+
+export const DataConfig = {
+  saleTitle: '分类总量',
+  saleIcon: 'el-icon-receiving',
+  saleCrement: '0%',
+  color: '#4bde97',
+  backgroundColor: '#edfcf5'
+}
