855bb0bf6e93ed2bd8661084fb544af62fce819d
swanine <haohao1500@foxmail.com>
2021-10-02 17:39:28 +0800

    feat: countup base component

---
+++ src/components/count-up/count-up.vue
@@ -0,0 +1,65 @@
+<template>
+  <span ref="counterRef" />
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref, watch } from 'vue'
+// http://inorganik.github.io/countUp.js/
+import { CountUp } from 'countup.js'
+import type { CountUpOptions } from 'countup.js'
+const defaultOptions: CountUpOptions = {
+  duration: 2, // 动画时长
+  separator: ',', // 千位分割
+  decimal: '.' // 小数分割
+}
+export default defineComponent({
+  components: {},
+  props: {
+    number: {
+      type: Number,
+      default: 0
+    },
+    option: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  setup(props) {
+    const counterRef = ref<HTMLDivElement | null>(null)
+    const instance = ref<CountUp | null>(null)
+
+    watch(
+      () => props.number,
+      () => {
+        update(props.number)
+      }
+    )
+    onMounted(() => {
+      createCounter()
+    })
+
+    const createCounter = () => {
+      if (!counterRef.value) return
+      const opts: CountUpOptions = Object.assign(defaultOptions, props.option)
+      instance.value = new CountUp(counterRef?.value, props.number, opts)
+      start()
+    }
+
+    const start = () => {
+      if (!instance.value) return
+      instance?.value.start()
+    }
+    const update = (number: number) => {
+      if (!instance.value) return
+      instance?.value.update(number)
+    }
+    return {
+      counterRef
+    }
+  }
+})
+</script>
+
+<style scoped lang="less"></style>
