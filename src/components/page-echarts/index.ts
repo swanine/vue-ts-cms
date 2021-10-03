8eaf617ad26822d310eafc06bfe227a58227d0af
swanine <haohao1500@foxmail.com>
2021-10-03 23:10:09 +0800

    feat: echarts component import idnex

--- src/components/page-echarts/index.ts
+++ src/components/page-echarts/index.ts
@@ -1,4 +1,15 @@
 import pieEcharts from './src/pie-echarts.vue'
 import roseEcharts from './src/rose-echarts.vue'
+import lineEcharts from './src/line-echarts.vue'
+import barEcharts from './src/bar-echarts.vue'
+import mapEcharts from './src/map-echarts.vue'
+import saleNumber from './src/sale-number.vue'
 
-export { pieEcharts, roseEcharts }
+export {
+  pieEcharts,
+  roseEcharts,
+  lineEcharts,
+  barEcharts,
+  mapEcharts,
+  saleNumber
+}
