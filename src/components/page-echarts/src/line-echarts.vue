172a13ce53ab268e6a6d13b10096250a0e5da717
swanine <haohao1500@foxmail.com>
2021-10-03 23:09:32 +0800

    feat: line echarts component

---
+++ src/components/page-echarts/src/line-echarts.vue
@@ -0,0 +1,51 @@
+<template>
+  <base-echart :options="options" height="300px"></base-echart>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+import baseEchart from '@/base-ui/base-echart/index'
+
+const props = defineProps<{
+  xLabel: string[]
+  yValue: any[]
+}>()
+
+const options = computed(() => {
+  return {
+    legend: {},
+    tooltip: {
+      tirgger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        snap: true
+      }
+    },
+    grid: {
+      right: 20,
+      bottom: 30,
+      top: 40
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: props.xLabel
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        name: '分别销量',
+        stack: '总量',
+        data: props.yValue,
+        type: 'line',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        }
+      }
+    ]
+  }
+})
+</script>
