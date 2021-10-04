7865ff1fb346f40b91cb958aa7a228c869b24b2b
swanine <haohao1500@foxmail.com>
2021-10-04 22:38:12 +0800

    feat: overview page text component

---
+++ src/components/overview-text/overview-text.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="text-container">
+    <li class="text-item" v-for="item in textArrs" :key="item.title">
+      <span>{{ item.title }}：</span>
+      <el-link type="primary" href="JavaScript:;">{{ item.desp }}</el-link>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  },
+  props: {
+    textArrs: {
+      type: Array
+    }
+  }
+})
+</script>
+
+<style scoped lang="less">
+.text-container {
+  padding: 0 20px;
+  .text-item {
+    margin: 10px 0;
+    &:nth-child(1) {
+      margin-top: 0;
+    }
+    &:nth-last-child(1) {
+      margin-bottom: 0;
+    }
+  }
+}
+</style>
