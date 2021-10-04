c0f82f2937762f30f903bc0e553081f67dc21e36
swanine <haohao1500@foxmail.com>
2021-10-04 22:38:40 +0800

    feat: map echarts component

---
+++ src/components/page-echarts/src/map-echarts.vue
@@ -0,0 +1,74 @@
+<template>
+  <base-echart :options="options" height="250px"></base-echart>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+import baseEchart from '@/base-ui/base-echart/index'
+import { IDataType } from '../types/type'
+import { convertData } from '../utils/convert-data'
+const props = defineProps<{
+  mapData: IDataType[]
+}>()
+
+const options = computed(() => {
+  return {
+    backgroundColor: '#fff',
+    tooltip: {
+      trigger: 'item',
+      formatter: function (params: any) {
+        return params.name + ' : ' + params.value[2]
+      }
+    },
+    visualMap: {
+      min: 0,
+      max: 60000,
+      left: 20,
+      bottom: 0,
+      calculable: true,
+      text: ['高', '低'],
+      inRange: {
+        color: ['rgb(70, 240, 252)', 'rgb(250, 220, 46)', 'rgb(245, 38, 186)']
+      },
+      textStyle: {
+        color: '#fff'
+      }
+    },
+    geo: {
+      map: 'china',
+      roam: 'scale',
+      emphasis: {
+        areaColor: '#f4cccc',
+        borderColor: 'rgb(9, 54, 95)',
+        itemStyle: {
+          areaColor: '#f4cccc'
+        }
+      }
+    },
+    series: [
+      {
+        name: '销量',
+        type: 'scatter',
+        coordinateSystem: 'geo',
+        data: convertData(props.mapData),
+        symbolSize: 12,
+        emphasis: {
+          itemStyle: {
+            borderColor: '#fff',
+            borderWidth: 1
+          }
+        }
+      },
+      {
+        type: 'map',
+        map: 'china',
+        geoIndex: 0,
+        aspectScale: 0.75,
+        tooltip: {
+          show: false
+        }
+      }
+    ]
+  }
+})
+</script>
