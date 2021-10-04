1aa4290f307ab15ae716a3cc448c38602713bf95
swanine <haohao1500@foxmail.com>
2021-10-04 22:39:01 +0800

    feat: pie echarts component

---
+++ src/components/page-echarts/src/pie-echarts.vue
@@ -0,0 +1,40 @@
+<template>
+  <base-echart :options="options"></base-echart>
+</template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+import baseEchart from '@/base-ui/base-echart/index'
+import { IDataType } from '../types/type'
+
+const props = defineProps<{
+  pieData: IDataType[]
+}>()
+
+const options = computed(() => {
+  return {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      orient: 'horizontal',
+      left: 'left'
+    },
+    series: [
+      {
+        name: '分类数据',
+        type: 'pie',
+        radius: '50%',
+        data: props.pieData,
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        }
+      }
+    ]
+  }
+})
+</script>
