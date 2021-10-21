954cef4966d3964c82184c51750a8258a178a3b7
swanine <haohao1500@foxmail.com>
2021-10-21 19:48:22 +0800

    feat: dependency change

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,6 +1,6 @@
-import type { App } from 'vue'
-import registerElement from './register-element'
+// import type { App } from 'vue'
+// import registerElement from './register-element'
 
-export function globalRegister(app: App): void {
-  app.use(registerElement)
-}
+// export function globalRegister(app: App): void {
+//   app.use(registerElement)
+// }
