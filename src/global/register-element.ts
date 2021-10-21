902571da83178e2e995c338b1dd31719a599a1f1
swanine <haohao1500@foxmail.com>
2021-10-21 19:48:08 +0800

    feat: dependency change

--- src/global/register-element.ts
+++ src/global/register-element.ts
@@ -1,12 +1,12 @@
-import type { App } from 'vue'
+// import type { App } from 'vue'
 
-import 'element-plus/theme-chalk/base.css'
-import { ElButton, ElInput, ElTabPane, ElTabs } from 'element-plus'
+// import 'element-plus/theme-chalk/base.css'
+// import { ElButton, ElInput, ElTabPane, ElTabs } from 'element-plus'
 
-const components = [ElButton, ElInput, ElTabs, ElTabPane]
+// const components = [ElButton, ElInput, ElTabs, ElTabPane]
 
-export default function (app: App): void {
-  for (const component of components) {
-    app.component(component.name, component)
-  }
-}
+// export default function (app: App): void {
+//   for (const component of components) {
+//     app.component(component.name, component)
+//   }
+// }
